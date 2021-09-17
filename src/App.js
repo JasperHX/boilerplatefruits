ffdb2ca6377849dd9c9452dd4e4bd90e65294e82
JasperHX <jasper-hendrikx@hotmail.com>
2021-09-17 18:17:05 +0200

    aanpassingformulier

--- src/App.js
+++ src/App.js
@@ -7,6 +7,10 @@ import ijsblokjes from './assets/ijsblokjes.jpg';
 import { ReactComponent as ShoppingCart } from './assets/winkelmandje.svg';
 
 function App() {
+
+    const [messageValue, setMessageValue] = React.useState('');
+    const [checkedTerms, toggleCheckedTerms] = React.useState(false);
+
     return (
         <>
         <nav>
@@ -39,6 +43,41 @@ function App() {
                 description="Een ijsblokje of ijsklontje is bevroren water in de vorm van een klein blokje. Het wordt gemaakt in een diepvriezer door water in een plastic vorm te laten bevriezen."
             />
     </main>
+            <footer>
+                <div className="form-container">
+                    <h2>Contactformulier</h2>
+
+                    <form>
+                        <input
+                        type='text'
+                        placeholder='Typ hier jouw bericht'
+                        name='message'
+                        className={messageValue.length > 20 ? 'input-error' : 'input-normal'}
+                        value={messageValue}
+                        onChange={(e) => setMessageValue(e.target.value)}
+                        />
+                        {messageValue.length > 20 && <p className="error-message">Dit bericht is te lang!</p>}
+
+                        <label htmlFor="terms-and-conditions">
+                            <input
+                                type="checkbox"
+                                name="terms-and-conditions"
+                                id="terms-and-conditions"
+                                checked={checkedTerms}
+                                onChange={() => toggleCheckedTerms(!checkedTerms)}
+                        />
+                            Ik ga akkoord met de algemene voorwaarden
+                        </label>
+
+                        <button
+                        disabled={!checkedTerms}
+                        type="submit"
+                        >
+                        Verstuur
+                        </button>
+                    </form>
+                </div>
+            </footer>
         </>
     );
 }
